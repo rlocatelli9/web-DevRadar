86e082888c95cc5f54710a0f5028ffd0f3b7cce2
Robson Locatelli <rlocatelli9@gmail.com>
2020-01-25 20:09:05 -0300

    Versao web com Dev.delete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import "./global.css";
+import Routes from "./routes";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
